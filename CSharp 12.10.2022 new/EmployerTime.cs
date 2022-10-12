27e71f77ff2bffe84e02d86f4bfd73e7d13bf583
FaZiO86 <91316222+FaZiO86@users.noreply.github.com>
2022-10-12 21:10:36 +0500

    Добавьте файлы проекта.

---
+++ CSharp 12.10.2022 new/EmployerTime.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_12._10._2022_new {
+    enum DayOfWeek {
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        fridat,
+        saturday,
+        sunday
+    }
+    class EmployerTime {
+        
+        private int[] workTime;
+        public EmployerTime() { 
+            workTime = new int[7];
+           
+        }
+        
+        public int this[int index] {
+            
+            set { if (value > 0 && value < 8)
+                    { workTime[index] = value; } 
+                else 
+                {  workTime[index] = 0; } 
+            }
+            get { return workTime[index]; }
+        }
+         
+        public void Print() {
+            foreach (var day in Enum.GetValues(typeof(DayOfWeek))) { 
+                
+                Console.WriteLine($"день {day}, отработано {workTime[(int)day]} часов");
+            }
+        }
+    }
+}
---
+++ CSharp 12.10.2022 new/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_12._10._2022_new {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            var BobTime = new EmployerTime();
+            BobTime.Print();
+            BobTime[2] = int.Parse(args[0]);
+            BobTime.Print();
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
